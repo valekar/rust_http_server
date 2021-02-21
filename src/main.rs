11e97c0cc4e33bcecf0792a30b88c07de902094a
Prashanth Valekar, Srinivas <srinivas.prashanth.valekar@sap.com>
2021-02-21 16:18:13 +0000

    securely serving static files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
-
 mod http;
 mod server;
 mod website_handler;
@@ -10,8 +10,10 @@ fn main() {
     //let get = Method::GET;
     //let post = Method::POST;
 
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
 
 // tow types of strings
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,14 +1,50 @@
 use super::server::Handler;
 use crate::http::{Method, Request, Response, StatusCode};
-pub struct WebsiteHandler;
+use std::fs;
+pub struct WebsiteHandler {
+    pub_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn pub_path(&self) -> &String {
+        &self.pub_path
+    }
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self {
+            pub_path: public_path,
+        }
+    }
+
+    pub fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", &self.pub_path(), file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(self.pub_path()) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack attempted {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::OK, Some("<h1>Root</h1>".to_string())),
+                "/" => Response::new(StatusCode::OK, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::OK, Some("Hello".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::OK, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
